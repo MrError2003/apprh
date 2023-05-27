3eb98273dbcc3bdd34badca6c681927f6d17d450
Juan David Zapata Marquez <jdzapatamar@cesde.net>
2023-05-27 08:04:06 -0500

    Publicacion

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include 'controller/head.php'
+?>
+
+<body>
+
+<?php
+include 'views\navbar.php';
+include 'views\login.php';
+?>
+
+</body>
+
+<?php
+include 'controller/scripts.php'
+?>
+
+</html>
