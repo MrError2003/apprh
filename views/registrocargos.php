c54e56e3a5da0cebdd92fe247313a0509a04449a
Juan David Zapata Marquez <jdzapatamar@cesde.net>
2023-06-13 14:48:53 -0500

    nnn

--- views/registrocargos.php
+++ views/registrocargos.php
@@ -40,50 +40,39 @@
 </div>
 
 <body>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <h1>Registro de Cargos</h1>
-                <form method="post" action="registro_cargos_controller.php">
-                    <div class="form-group">
-                        <label for="cargo">Cargo:</label>
-                        <input type="text" class="form-control" id="cargo" name="cargo" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="area">Área:</label>
-                        <input type="text" class="form-control" id="area" name="area" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="tipocontrato">Tipo de Contrato:</label>
-                        <input type="text" class="form-control" id="tipocontrato" name="tipocontrato" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="salariobruto">Salario Bruto:</label>
-                        <input type="number" class="form-control" id="salariobruto" name="salariobruto" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="recargos">Recargos:</label>
-                        <input type="number" class="form-control" id="recargos" name="recargos" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="prestaciones">Prestaciones:</label>
-                        <input type="number" class="form-control" id="prestaciones" name="prestaciones" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="salarioneto">Salario Neto:</label>
-                        <input type="number" class="form-control" id="salarioneto" name="salarioneto" required>
-                    </div>
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary" name="registrar">Registrar</button>
-                    </div>
-                </form>
-            </div>
+    <form method="post" action="registro_cargos_controller.php">
+        <div>
+            <label for="cargo">Cargo:</label>
+            <input type="text" id="cargo" name="cargo" required>
         </div>
-    </div>
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+        <div>
+            <label for="cargo">Area:</label>
+            <input type="text" id="area" name="area" required>
+        </div>
+        <div>
+            <label for="tipocontrato">Tipo de Contrato:</label>
+            <input type="text" id="tipocontrato" name="tipocontrato" required>
+        </div>
+        <div>
+            <label for="salariobruto">Salario Bruto:</label>
+            <input type="number" id="salariobruto" name="salariobruto" required>
+        </div>
+        <div>
+            <label for="recargos">Recargos:</label>
+            <input type="number" id="recargos" name="recargos" required>
+        </div>
+        <div>
+            <label for="prestaciones">Prestaciones:</label>
+            <input type="number" id="prestaciones" name="prestaciones" required>
+        </div>
+        <div>
+            <label for="salarioneto">Salario Neto:</label>
+            <input type="number" id="salarioneto" name="salarioneto" required>
+        </div>
+        <div>
+            <button type="submit" name="registrar">Registrar</button>
+        </div>
+    </form>
 </body>
 
 </html>
