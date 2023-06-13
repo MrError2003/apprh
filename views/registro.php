1074d93fe9c4273bc67d0edb6e0d4a4e67a9b7b4
Juan David Zapata Marquez <jdzapatamar@cesde.net>
2023-06-13 14:22:51 -0500

    modificacion usuario nomina

--- views/registro.php
+++ views/registro.php
@@ -9,14 +9,11 @@
       include '../controller/conexion.php';
       if (isset($_POST['registrarse'])) {
         $identificacion = $_POST['identificacion'];
-        $nombre = $_POST['nombre']; //escampar el input en una variable     
         $email = $_POST['email'];
         $password = $_POST['password'];
-        $estado = "ACTIVO";
-        $fechacreacion = date("Y-m-d");
 
-        $insert = mysqli_query($con, "INSERT INTO empleados(nombre,identificacion,email,pasword,estado, fechacreacion)
-          VALUES ('$nombre','$identificacion','$email','$password','$estado','$fechacreacion')");
+        $insert = mysqli_query($con, "INSERT INTO usuariosnomina(nombre,identificacion,email,pasword,estado, fechacreacion)
+          VALUES ('$identificacion','$email','$password')");
 
         if ($insert) {
           echo '<div class="alert alert-succer" role="alert">
@@ -32,24 +29,20 @@
 
     <form method="post">
       <div class="mb-3">
-        <label for="registerName" class="form-label">Nombre</label>
-        <input type="text" class="form-control" id="registerName" placeholder="Nombre" name="nombre" required>
+        <label for="registerIdentificacion" class="form-label">Identificacion</label>
+        <input type="text" class="form-control" id="identificacion" placeholder="Identificacion" name="identificacion" required>
       </div>
       <div class="mb-3">
-        <label for="registerEmail" class="form-label">Correo electrónico</label>
-        <input type="email" class="form-control" id="registerEmail" placeholder="ejemplo@correo.com" name="email" required>
+        <label for="registerEmail" class="form-label">Email corporativo</label>
+        <input type="email" class="form-control" id="corpemail" placeholder="ejemplo@correo.com" name="corpemail" required>
       </div>
       <div class="mb-3">
         <label for="registerPassword" class="form-label">Contraseña</label>
         <input type="password" class="form-control" id="registerPassword" placeholder="Contraseña" name="password" required>
       </div>
-      <div class="mb-3">
-        <label for="registerIdentificacion" class="form-label">Identificacion</label>
-        <input type="text" class="form-control" id="registerPassword" placeholder="Identificacion" name="identificacion" required>
-      </div>
 
       <div class="text-center">
-        <button type="submit" class="btn btn-primary" name="registrarse">Registrarse</button>
+        <button type="submit" class="btn btn-primary" name="registrarse">Registro Usuario nomina</button>
       </div>
     </form>
 
