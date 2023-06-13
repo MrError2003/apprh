4a2847d347aee1c24009c5398246a5323edeee04
Juan David Zapata Marquez <jdzapatamar@cesde.net>
2023-06-13 16:36:57 -0500

    diseño registro usuario

--- views/registro.php
+++ views/registro.php
@@ -1,9 +1,7 @@
 <div class="container">
   <div class="row">
-
     <div class="tab-pane fade" id="register">
       <h3 class="mb-4">Registrarse</h3>
-
 
       <?php
       include '../controller/conexion.php';
@@ -16,12 +14,12 @@
           VALUES ('$identificacion','$email','$password')");
 
         if ($insert) {
-          echo '<div class="alert alert-succer" role="alert">
-              DATOS GUARDADOS CON EXITO
+          echo '<div class="alert alert-success" role="alert">
+              DATOS GUARDADOS CON ÉXITO
               <a href="../index.php" class="btn btn-warning">REGRESAR AL INICIO </a>
               </div>';
         } else {
-          echo "error al guardar";
+          echo "Error al guardar";
         }
       }
       ?>
@@ -29,12 +27,12 @@
 
     <form method="post">
       <div class="mb-3">
-        <label for="registerIdentificacion" class="form-label">Identificacion</label>
-        <input type="text" class="form-control" id="identificacion" placeholder="Identificacion" name="identificacion" required>
+        <label for="registerIdentificacion" class="form-label">Identificación</label>
+        <input type="text" class="form-control" id="registerIdentificacion" placeholder="Identificación" name="identificacion" required>
       </div>
       <div class="mb-3">
         <label for="registerEmail" class="form-label">Email corporativo</label>
-        <input type="email" class="form-control" id="corpemail" placeholder="ejemplo@correo.com" name="corpemail" required>
+        <input type="email" class="form-control" id="registerEmail" placeholder="ejemplo@correo.com" name="email" required>
       </div>
       <div class="mb-3">
         <label for="registerPassword" class="form-label">Contraseña</label>
@@ -42,11 +40,8 @@
       </div>
 
       <div class="text-center">
-        <button type="submit" class="btn btn-primary" name="registrarse">Registro Usuario nomina</button>
+        <button type="submit" class="btn btn-primary" name="registrarse">Registro Usuario nómina</button>
       </div>
     </form>
-
-
-    
   </div>
 </div>
