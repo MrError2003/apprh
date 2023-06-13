b1225932a8ef4b9893e57ca377080c1160845102
Juan David Zapata Marquez <jdzapatamar@cesde.net>
2023-06-13 14:32:55 -0500

    ensayo nuevo login

--- views/login.php
+++ views/login.php
@@ -1,20 +1,37 @@
-<div class="container">
-
-    <div class="row">
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Formulario de Inicio de Sesión</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+</head>
+<body>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <h1>Iniciar Sesión</h1>
         <form>
-            <div class="mb-3">  
-                <label for="exampleInputEmail1" class="form-label">Correo electronico</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-                <input type="password" class="form-control" name="password" id="exampleInputPassword1">
-            </div>
-            <button type="submit" class="btn btn-primary" name="iniciar_sesion">Iniciar</button>
-            <button type="submit" class="btn btn-primary" name="registro" onclick="">Registro</button>
+          <div class="form-group">
+            <label for="identificacion">Identificación</label>
+            <input type="text" class="form-control" id="identificacion" placeholder="Ingrese su identificación" name="identificacion">
+          </div>
+          <div class="form-group">
+            <label for="correo">Correo Electrónico</label>
+            <input type="email" class="form-control" id="correo" placeholder="Ingrese su correo electrónico" name="email">
+          </div>
+          <div class="form-group">
+            <label for="contrasena">Contraseña</label>
+            <input type="password" class="form-control" id="contrasena" placeholder="Ingrese su contraseña" name="contraseña">
+          </div>
+          <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
         </form>
-
+      </div>
     </div>
+  </div>
 
-</div>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
