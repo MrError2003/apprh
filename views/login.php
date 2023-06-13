5f33ce565d80e3ebc2c1bff08468c752e7d318b9
Juan David Zapata Marquez <jdzapatamar@cesde.net>
2023-06-13 14:45:39 -0500

    diseño  formulario reg cargo

--- views/login.php
+++ views/login.php
@@ -7,6 +7,8 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 </head>
 <body>
+    <br>
+    <br>
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-6">
--- views/registrocargos.php
+++ views/registrocargos.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    
+
 </head>
 
 <div class="tab-pane fade" id="register">
@@ -13,6 +13,7 @@
 
     if (isset($_POST['registrar'])) {
         $cargo = $_POST['cargo'];
+        $area = $_POST['area'];
         $tipocontrato = $_POST['tipocontrato'];
         $salariobruto = $_POST['salariobruto'];
         $recargos = $_POST['recargos'];
@@ -38,37 +39,51 @@
 
 </div>
 
-
 <body>
-    <form method="post" action="registro_cargos_controller.php">
-        <div>
-            <label for="cargo">Cargo:</label>
-            <input type="text" id="cargo" name="cargo" required>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <h1>Registro de Cargos</h1>
+                <form method="post" action="registro_cargos_controller.php">
+                    <div class="form-group">
+                        <label for="cargo">Cargo:</label>
+                        <input type="text" class="form-control" id="cargo" name="cargo" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="area">Área:</label>
+                        <input type="text" class="form-control" id="area" name="area" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="tipocontrato">Tipo de Contrato:</label>
+                        <input type="text" class="form-control" id="tipocontrato" name="tipocontrato" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="salariobruto">Salario Bruto:</label>
+                        <input type="number" class="form-control" id="salariobruto" name="salariobruto" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="recargos">Recargos:</label>
+                        <input type="number" class="form-control" id="recargos" name="recargos" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="prestaciones">Prestaciones:</label>
+                        <input type="number" class="form-control" id="prestaciones" name="prestaciones" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="salarioneto">Salario Neto:</label>
+                        <input type="number" class="form-control" id="salarioneto" name="salarioneto" required>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary" name="registrar">Registrar</button>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div>
-            <label for="tipocontrato">Tipo de Contrato:</label>
-            <input type="text" id="tipocontrato" name="tipocontrato" required>
-        </div>
-        <div>
-            <label for="salariobruto">Salario Bruto:</label>
-            <input type="number" id="salariobruto" name="salariobruto" required>
-        </div>
-        <div>
-            <label for="recargos">Recargos:</label>
-            <input type="number" id="recargos" name="recargos" required>
-        </div>
-        <div>
-            <label for="prestaciones">Prestaciones:</label>
-            <input type="number" id="prestaciones" name="prestaciones" required>
-        </div>
-        <div>
-            <label for="salarioneto">Salario Neto:</label>
-            <input type="number" id="salarioneto" name="salarioneto" required>
-        </div>
-        <div>
-            <button type="submit" name="registrar">Registrar</button>
-        </div>
-    </form>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 </body>
 
 </html>
